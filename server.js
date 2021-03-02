4e66d3fa6d4edd9407ec50128a51adc22b2f6b5d
UPye <75226236+UPye@users.noreply.github.com>
2021-03-02 06:51:18 -0500

    Added ability to filter by ID

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -41,7 +42,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 
 
@@ -51,6 +55,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(PORT, () => {
